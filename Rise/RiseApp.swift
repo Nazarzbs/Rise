4eeec64bccf13851195962f953af2a8aad609e40
Nazar <famina1998@gmail.com>
2025-06-21 18:19:52 +0300

    Initial Commit

---
+++ Rise/RiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiseApp.swift
+//  Rise
+//
+//  Created by Nazar on 21/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
